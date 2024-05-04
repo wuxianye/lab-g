d9ba1e95b6a23b330a298226ff75c926bbf4690e
wuxianye <164884853+wuxianye@users.noreply.github.com>
2024-05-04 14:44:35 +0100

    Add files via upload

--- task_g.c
+++ task_g.c
@@ -5,7 +5,7 @@ int main(void) {
 	// Your code below here
     FILE* fptr = fopen("numbers.txt", "r");
     if (fptr == NULL) {
-        printf("Error!\n");
+        printf("Error!");
         return 1;
     }
 
@@ -16,7 +16,7 @@ int main(void) {
     }
 
     if (count < 20) {
-        printf("Error!\n");
+        printf("Error!");
         fclose(fptr);
         return 1;
     }
@@ -27,10 +27,10 @@ int main(void) {
     find_max_less_than_10(numbers, 20, &max);
 
     if (max == -1) {
-        printf("Error!\n");
+        printf("Error!");
     }
     else {
-        printf("%.2f\n", max);
+        printf("%.2f", max);
     }
 
 
